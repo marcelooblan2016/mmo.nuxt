861a51f320d395dc4b58ab1337ad3d822948a073
Marcelo Oblan <marcelo.oblan2016@gmail.com>
2023-11-04 20:06:11 +0800

    update followup

--- components/Header/Menu.vue
+++ components/Header/Menu.vue
@@ -14,7 +14,7 @@
 					@click.prevent="isOpen = true;"
 					>
 						<span class="sr-only">Open main menu</span> 
-						<svg class="w-6 h-6 text-white md:text-blue-800 font-bold" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+						<svg class="w-6 h-6 text-white md:text-gray-800 font-bold" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
 				    </button>
                 </template>
 				<template v-if="isOpen == true">
