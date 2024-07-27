061b96935430a37402fd5f135293030189320468
Marcelo Oblan <marcelo.oblan2016@gmail.com>
2024-07-27 10:18:30 +0800

    update

--- mixins/global.js
+++ mixins/global.js
@@ -120,7 +120,7 @@ export default {
                     description: [
                         "Team member as a developer at documerge.ai primarily focuses on integration tasks, implementing new features, and bug fixing.",
                         "Led the upgrade of large legacy applications from Nuxt 2 to Nuxt 3, enhancing performance, maintainability, and future-proofing the technology stack.",
-                        "Skills: Laravel, Vue.js, Inertia.js, MySql, Tailwind, Git"
+                        "Skills: Laravel, Vue.js, Inertia.js, Nuxt.js (echo, pusher), MySql, Tailwind, Git"
                     ]
                 },
                 {
